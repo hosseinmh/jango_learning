0e20dc26d9daf0822f43e65c67d3a6f79a8fa5d5
hossseinmh <akhoundi22@gmail.com>
2017-01-30 21:00:13 +0330

    jango is installed and todo file update

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hossein/Desktop/Project/jango_learning/jango_learning/.venv/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
