3b43a6aba2e9c69983221bf367cc58ef109d35e0
hossseinmh <akhoundi22@gmail.com>
2017-03-02 21:23:49 +0330

    now we redirect a user that not login

--- jango_learning/web/views.py
+++ jango_learning/web/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 from .models import PostModel
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 def post_model(request):
+
     qs = PostModel.objects.all()
     print(request.user)
+
+    context = {
+        "object_list": qs
+    }
+
     if request.user.is_authenticated():
+        template = "web/index_list.html"
         print ("logged in ")
-    else :
+    else:
+        template = "web/404.html"
         print ("not logged in ")
 
-
-    # return HttpResponse("some new data")
-    template = "web/index_list.html"
-    context = {
-         "object_list": qs
-    }
     return render(request, template, context)
